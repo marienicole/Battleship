9c626252d61569335b1e7b26fbec89f4c5f755f4
Marie Morin <mmorin@dataminr.com>
2018-09-13 13:21:52 -0600

    commented out connection lines in favor of requests

--- battleship/client.py
+++ battleship/client.py
@@ -7,10 +7,10 @@ import requests, argparse, socket, http.client
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Define server IP, connection port and user\'s board.')
-    parser.add_argument('server', action='store')#, description='IP of the server')
-    parser.add_argument('port', action='store')#, description='Port to connect to server')
-    parser.add_argument('x_loc', action='store')#, description='X-axis location of fire')
-    parser.add_argument('y_loc', action='store')#, description='Y-axis location of fire')
+    parser.add_argument('server', action='store')
+    parser.add_argument('port', action='store')
+    parser.add_argument('x_loc', action='store')
+    parser.add_argument('y_loc', action='store')
     args = parser.parse_args()                  #Had to comment these out to run
 
     server_ip = args.server
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
     r = requests.post('http://%s:%s?x=%s&y=%s' %('localhost', port, x_loc, y_loc))
     print(r)
-    conn = http.client.HTTPConnection(host, port)
-    conn.request("POST", 'http://%s:%s?x=%s&y=%s' % (host, port, x_loc, y_loc))
+    #conn = http.client.HTTPConnection(host, port)
+    #conn.request("POST", 'http://%s:%s?x=%s&y=%s' % (host, port, x_loc, y_loc))
     #response = conn.getresponse() For when we get the responses working
     #data = response.read()
-    conn.close()
+    #conn.close()
