8ab09be01c7083a761a8626fabf0b4fe207a350d
Hugh Jackovich <hughman25@gmail.com>
2018-09-12 23:57:56 -0600

    Client can send HTTP to server

--- battleship/client.py
+++ battleship/client.py
@@ -3,17 +3,27 @@
 CSCI 466: Networks Programming Assignment 1.
 Authored by Marie Morin and Hughe Jackovich.
 '''
-import requests, argparse, socket
+import requests, argparse, socket, http.client
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Define server IP, connection port and user\'s board.')
-    parser.add_argument('server', action='store', description='IP of the server')
-    parser.add_argument('port', action='store', description='Port to connect to server')
-    parser.add_argument('x_loc', action='store', description='X-axis location of fire')
-    parser.add_argument('y_loc', action='store', description='Y-axis location of fire')
-    args = parser.parse_args()
+    parser.add_argument('server', action='store')#, description='IP of the server')
+    parser.add_argument('port', action='store')#, description='Port to connect to server')
+    parser.add_argument('x_loc', action='store')#, description='X-axis location of fire')
+    parser.add_argument('y_loc', action='store')#, description='Y-axis location of fire')
+    args = parser.parse_args()                  #Had to comment these out to run
 
     server_ip = args.server
     port = args.port
     x_loc = args.x_loc
     y_loc = args.y_loc
+    host = socket.gethostname()
+
+    #serv_response = requests.post('http://%s:%s?x=%s&y=%s' % (host, port, x_loc, y_loc))
+    conn = http.client.HTTPConnection(host, port)
+    conn.request("POST", 'http://%s:%s?x=%s&y=%s' % (host, port, x_loc, y_loc))
+    #response = conn.getresponse() For when we get the responses working
+    #data = response.read()
+    conn.close()
+
+
--- battleship/server.py
+++ battleship/server.py
@@ -4,6 +4,18 @@ CSCI 466: Networks Programming Assignment 1.
 Authored by Marie Morin and Hughe Jackovich.
 '''
 import requests, argparse, socket
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    #may need a set_header function?
+    #Also default here to return HTTP OK?
+    def do_POST(self):
+        print("Handles POST requests")
+        #this works from server -> client
+    def do_GET(self):
+        print("Handles GET requests")
+        #probably return the board.txt?
+    #This'll handle HTTP requests/responses by invoking BaseHTTPRequestHandler
 
 
 def main():
@@ -12,14 +24,24 @@ def main():
     parser.add_argument('board', action='store')
     args = parser.parse_args()
 
-    port = args.port
+    port = int(args.port) #may wanna put try on this later
     board_loc = args.board
+    host = socket.gethostname() #Don't think this means we have to close the socket
     read_board(board_loc)
+
+    initServer(host, port)
+
 
 def read_board(board_loc):
     board = open(board_loc, "r")
     lines = board.readlines()
     for line in lines:
         print(line)
+
+def initServer(host, port):
+    address = (host, port) #tuple forms address of the server
+    handler = RequestHandler
+    http_client = HTTPServer(address, handler)
+    http_client.serve_forever()
 
 main()
