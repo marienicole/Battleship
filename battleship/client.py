485e255fd606069203a2b90c42764c3b6743bda5
wizmariefa <mariecolee1@gmail.com>
2018-09-12 18:24:45 -0600

    added requirements, reads in client board and prints

--- battleship/client.py
+++ battleship/client.py
@@ -3,4 +3,17 @@
 CSCI 466: Networks Programming Assignment 1.
 Authored by Marie Morin and Hughe Jackovich.
 '''
-import requests, argparse
+import requests, argparse, socket
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Define server IP, connection port and user\'s board.')
+    parser.add_argument('server', action='store', description='IP of the server')
+    parser.add_argument('port', action='store', description='Port to connect to server')
+    parser.add_argument('x_loc', action='store', description='X-axis location of fire')
+    parser.add_argument('y_loc', action='store', description='Y-axis location of fire')
+    args = parser.parse_args()
+
+    server_ip = args.server
+    port = args.port
+    x_loc = args.x_loc
+    y_loc = args.y_loc
--- battleship/server.py
+++ battleship/server.py
@@ -3,4 +3,23 @@
 CSCI 466: Networks Programming Assignment 1.
 Authored by Marie Morin and Hughe Jackovich.
 '''
-import requests, argparse
+import requests, argparse, socket
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Define connection port and users board.')
+    parser.add_argument('port', action='store')
+    parser.add_argument('board', action='store')
+    args = parser.parse_args()
+
+    port = args.port
+    board_loc = args.board
+    read_board(board_loc)
+
+def read_board(board_loc):
+    board = open(board_loc, "r")
+    lines = board.readlines()
+    for line in lines:
+        print(line)
+
+main()
